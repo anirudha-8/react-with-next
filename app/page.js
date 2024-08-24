77ec5e2b60baec0ea8f988c550451f0bdf80e153
SHADOW GAMING <19360346-SHADOWGAMING57@users.noreply.replit.com>
2024-08-24 16:35:36 +0000

    implement ajax or fetching api for random images

--- app/page.js
+++ app/page.js
@@ -5,9 +5,34 @@ import React, { useState } from "react";
 import Header from "../Components/Header";
 
 const page = () => {
+  const [images, setImages] = useState([]);
+  const getImages = async () => {
+    try {
+      const response = await fetch("https://picsum.photos/v2/list");
+      const data = await response.json();
+      setImages(data);
+      console.log(data);
+    } catch (error) {
+      console.log("Image not found");
+    }
+  };
   return (
     <>
-      <h1>This is Home Page</h1>
+      <div className="container">
+        <button onClick={getImages}>Get Images</button>
+        <div className="image-gallery">
+          {images.map((img) => {
+            return (
+              <img
+                id={img.id}
+                src={img.download_url}
+                alt={img.author}
+                width="200"
+              />
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
